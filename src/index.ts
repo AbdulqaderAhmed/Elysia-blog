c2df818ad820534e279f4f1f894338c34676efcc
AbdulqaderAhmed <abdulqaderahmed32@gmail.com>
2024-05-08 14:59:47 +0300

    openai swagger documentation added

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,17 @@ import { Elysia } from "elysia";
 import { dbConnection } from "./config/dbConnection";
 import { Blog } from "./model/blog.model";
 import { blogRoute } from "./routes/blog.routes";
+import { swagger } from "@elysiajs/swagger";
 
 // config
 dbConnection();
 const port: number = Number(Bun.env.SERVER_PORT);
 
-const app = new Elysia().decorate("Blog", Blog).use(blogRoute).listen(port);
+const app = new Elysia()
+  .decorate("Blog", Blog)
+  .use(swagger())
+  .use(blogRoute)
+  .listen(port);
 
 console.log(
   `🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`
